6b86bec165b2f47c14a8b34883c35c59276e796d
medeotl <fasiellon@yahoo.it>
2024-02-14 18:16:44 +0100

    Refactoring iniziale aggiunte funzioni getListaSillabate(), gestisciSillabata() e isParagrafoMonco()

--- epubcleanerCLI.py
+++ epubcleanerCLI.py
@@ -17,7 +17,52 @@ import shutil
 import re
 from bs4 import BeautifulSoup
 
-def unisciParagrafi():
+def getListaSillabate(paragrafo):
+    """ 'cattura' la lista di sillabate da controllare in paragrafo """
+
+    lista_sillabate = re.findall( r"\w+(?:-[\w]+)+", paragrafo, re.U )
+    for sillabata in lista_sillabate:
+        if sillabata.lower() in whitelist:
+            print( f"sillabata presente nella WHITELIST: {sillabata} \n" )
+            lista_sillabate.remove(sillabata)
+        if sillabata.lower() in keeplist:
+            print( f"sillabata presente nella KEEPLIST: {sillabata} \n")
+            lista_sillabate.remove(sillabata)
+    return lista_sillabate
+
+def gestisciSillabata( p_tag, sillabata):
+    """ gestisce la sillabata in base all'indicazione dell'utente """
+
+    print( f"Correggo la sillabata corrente ({sillabata})? [Sì|no|whitelist] ")
+    scelta_utente = input( "Scelta: [S|n|w]: " )
+
+    match scelta_utente:
+        case "n": # keeplist!
+            keeplist.append( sillabata.lower() )
+            print (f"{sillabata} aggiunta alla lista delle sillabate da mantenere nell'ebook")
+        case "w": # whitelist!
+            whitelist.append( sillabata.lower() )
+            print (f"{sillabata} aggiunta alla whitelist")
+        case _: # correggo la sillabata
+            cambiata = sillabata.replace( '-', '' )
+            for stringa in p_tag.strings:
+                if sillabata in stringa:
+                    nuova_stringa = stringa.replace( sillabata, cambiata )
+                    stringa.replace_with( nuova_stringa )
+                    break
+
+def isParagrafoMonco( paragrafo ):
+    """ controlla se il paragrafo è 'monco' """
+
+    if paragrafo == "" :
+        return False
+
+    if re.match( r"[^.…!?:»”]", paragrafo[-1], re.U ):
+        return True
+    else:
+        return False
+
+def unisciParagrafi( zuppa ):
     # unisce i paragrafi monchi del file html
     # i paragrafi finiscono solitamente con "." o "?" o":" o "»" o "”" o "…" o "!"
         # es. <p>Sto programmando. </p>
@@ -30,93 +75,53 @@ def unisciParagrafi():
     # i paragrafi non dovrebbero finire con "-"
         # es. <p>corregeva le sillaba-</p><p>te e i paragrafi monchi.</p>
 
-    for file_corrente in lista_file:
-        print( f"\n--- OPERO SU {file_corrente} ---" )
-        zuppa = BeautifulSoup( open( working_dir + file_corrente ), "html.parser" )
+    # controllo paragrafi monchi in file corrente
+    lista_p = zuppa.find_all( "p" )
+    for p_tag, p_tag_next in zip( lista_p[:-1], lista_p[1:] ):
+        paragrafo = p_tag.text.rstrip()
+        # check paragrafo monco
+        if isParagrafoMonco( paragrafo ):
+            # check paragrafo monco sillabato
+            if paragrafo.endswith( "-" ):
+                # cancello l'a capo
+                p_tag_next.decompose()
+                # imposto p_tag_next al valore corretto
+                p_tag_next = p_tag.findNextSibling()
 
-        # controllo paragrafi monchi in file corrente
-        lista_p = zuppa.find_all( "p" )
-        for p_tag, p_tag_next in zip( lista_p[:-1], lista_p[1:] ):
-            paragrafo = p_tag.text.rstrip()
-            if paragrafo == "" :
+            if p_tag_next.text.rstrip() == "" :
+                # TODO controllare utilità di questo passaggio
                 continue
-            # check paragrafo monco
-            if re.match( r"[^.…!?:»”]", paragrafo[-1], re.U ):
-                # check paragrafo monco sillabato
-                if paragrafo.endswith( "-" ):
-                    # cancello l'a capo
-                    p_tag_next.decompose()
-                    # imposto p_tag_next al valore corretto
-                    p_tag_next = p_tag.findNextSibling()
 
-                if p_tag_next.text.rstrip() == "" :
-                    continue
+            # richiedo valutazione dell'utente
+            print( "\n@@@ trovato paragrafo errato: " )
+            print( repr(paragrafo) )
+            print( repr( p_tag_next.text.rstrip() ))
+            unificare = input( "unifico i paragrafri? [S|n]: ")
+            if unificare != 'n':
+                # unisco i due paragrafi
+                print( "---DEBUG: Unifico i paragrafi ---" )
+                while p_tag_next.contents != []:
+                    p_tag.append( p_tag_next.contents[0] )
+                # elimino secondo paragrafo e pulisco
+                p_tag_next.decompose()
+                p_tag.smooth()
 
-                # richiedo valutazione dell'utente        
-                print( "\n@@@ trovata paragrafo errato: \n{}".format( repr(paragrafo) ))
-                print( repr( p_tag_next.text.rstrip() ))
-                unificare = input( "unifico i paragrafri? [S|n]: ")
-                if unificare != 'n':
-                    # unisco i due paragrafi
-                    print( "---DEBUG: Unifico i paragrafi ---" )
-                    while p_tag_next.contents != []:
-                        p_tag.append( p_tag_next.contents[0] )
-                    # elimino secondo paragrafo e pulisco
-                    p_tag_next.decompose()
-                    p_tag.smooth()
 
-        # salvo eventuali modifiche
-        zuppa_originale = BeautifulSoup( open( working_dir + file_corrente ),
-                                         "html.parser" )
-        if zuppa != zuppa_originale:
-            salvaModifiche( zuppa, file_corrente)
+def correggiSillabate( zuppa ):
+    """ cerca e corregge sillabate errate """
 
-def correggiSillabate():
-    # check sillabata sbagliata
+    # controllo paragrafi contenenti le sillabate
+    for p_tag in zuppa.find_all( "p" ):
+        paragrafo = p_tag.text
+        for sillabata in getListaSillabate( paragrafo ):
+            # isolo frase del paragrafo
+            for frase in paragrafo.split( "." ):
+                if sillabata in frase:
+                    print( f"FRASE: {frase.lstrip()}." )
+                    break
+            gestisciSillabata( p_tag, sillabata )
 
-    for file_corrente in lista_file:
-        print( f"--------              OPERO SU {file_corrente}            --------\n" )
-        zuppa = BeautifulSoup( open( working_dir + file_corrente ), "html.parser" )
 
-        # controllo paragrafi contenenti le sillabate
-        for p_tag in zuppa.find_all( "p" ):
-            paragrafo = p_tag.text
-            for sillabata in re.findall( r"\w+(?:-[\w]+)+", paragrafo, re.U ):
-                # whitelist?
-                if sillabata.lower() in whitelist:
-                    print( f"sillabata nella WHITELIST: {sillabata} \n" )
-                    continue
-                # keeplist?
-                if sillabata.lower() in keeplist:
-                    print( f"sillabata nella KEEPLIST: {sillabata} \n")
-                    continue
-                # sillabata da gestire
-                for frase in paragrafo.split( "." ):
-                    if sillabata in frase:
-                        print( "FRASE: " + frase.lstrip() + "." )
-                        break
-                print( f"Correggo la sillabata corrente ({sillabata})? [Sì|no|whitelist] ")
-                fix_sillabata = input( "Scelta: [S|n|w]: ")
-                match fix_sillabata:
-                    case "n": # keeplist!
-                        keeplist.append( sillabata.lower() )
-                        print (f"{sillabata} aggiunta alla lista delle sillabate da mantenere nell'ebook")
-                    case "w": # whitelist!
-                        whitelist.append( sillabata.lower() )
-                        print (f"{sillabata} aggiunta alla whitelist")
-                    case _: # correggo la sillabata
-                        cambiata = sillabata.replace( '-', '' )
-                        for stringa in p_tag.strings:
-                            if sillabata in stringa:
-                                nuova_stringa = stringa.replace( sillabata, cambiata )
-                                stringa.replace_with( nuova_stringa )
-                                break
-        # fine controllo paragrafi, salvo eventuali modifiche
-        zuppa_originale = BeautifulSoup( open( working_dir + file_corrente ),
-                                         "html.parser" )
-        if zuppa != zuppa_originale:
-            salvaModifiche( zuppa, file_corrente)
-    # fine elaborazione
 
 def salvaModifiche( zuppa, file_html ):
     # salva le modifiche apportate nel file html
@@ -193,11 +198,18 @@ for file in os.listdir( working_dir ):
 lista_file.sort()
 print( "lista file: {}\n".format( lista_file ))
 
-
 # controllo paragrafi monchi?
 check_paragrafi = input( "Eseguo controllo su paragrafi monchi? [S|n]: ")
 if check_paragrafi != 'n':
-    unisciParagrafi()
+    for file_corrente in lista_file:
+        print( f"\n--- OPERO SU {file_corrente} ---" )
+        zuppa = BeautifulSoup( open( working_dir + file_corrente ), "html.parser" )
+        unisciParagrafi( zuppa )
+        # salvo eventuali modifiche
+        zuppa_originale = BeautifulSoup( open( working_dir + file_corrente ),
+                                         "html.parser" )
+        if zuppa != zuppa_originale:
+            salvaModifiche( zuppa, file_corrente)
 
 # controllo sillabate errate?
 check_sillabate = input( "Eseguo controllo su sillabate errate? [S|n]: ")
@@ -218,10 +230,21 @@ if check_sillabate != 'n':
     # creo lista (vuota per ora) delle sillabate da lasciare per l'epub corrente
     keeplist = [] 
     
-    correggiSillabate()
+    # eseguo il controllo
+    for file_corrente in lista_file:
+        print( f"--------              OPERO SU {file_corrente}            --------\n" )
+        zuppa = BeautifulSoup( open( working_dir + file_corrente ), "html.parser" )
+        correggiSillabate( zuppa )
+        # salvo eventuali modifiche
+        zuppa_originale = BeautifulSoup( open( working_dir + file_corrente ),
+                                         "html.parser" )
+        if zuppa != zuppa_originale:
+            salvaModifiche( zuppa, file_corrente)
     salvaWhitelist()
 
 ricreaEpub()
 
 
 # TODO: se ho due p monchi consecutivi, l'unione dei due sarò ancora monco.
+
+
